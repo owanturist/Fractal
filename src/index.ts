0271100bb9b2fa704d17d7e6d7cee8e3d113f23e
Anton Ovechkin <owanturist@gmail.com>
2017-04-12 16:22:19 +0700

    Added shortcuts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-interface MaybePattern<T, U> {
+export interface MaybePattern<T, U> {
     Nothing(): U,
     Just(a: T): U
 }
 
-interface Maybe<T> {
+export interface Maybe<T> {
     of<U>(a: U): Maybe<U>;
     map<U>(f: (a: T) => U): Maybe<U>;
     ap<U>(b: Maybe<(a: T) => U>): Maybe<U>;
@@ -77,37 +77,6 @@ class Nothing<T> implements Maybe<T> {
     }
 }
 
-const bar = new Just(13);
+export const _Just = <T>(value: T) => new Just(value);
 
-const t = new Just(123);
-
-type Foo = Maybe<{
-    bar: {
-        baz: Maybe<number>
-    }
-}>
-
-const foo: Foo = new Just({
-    bar: {
-        baz: new Just(2)
-    }
-});
-
-const f: (a: number) => string = (a: number):string => a * 12 + '';
-
-const mBaz = foo
-    .chain(a => a.bar.baz)
-    .map(a => (b: number):((c: number) => number) => (c: number) => a * b * c)
-    .chain(bar.map)
-    .chain(t.map)
-    .map(a => a + 100)
-    .ap(bar.map(a => (b: number) => a * b))
-    .orElse(() => new Just(213))
-    .cata({
-        Nothing() {
-            return "321";
-        },
-        Just(a) {
-            return a * 2 + "123";
-        }
-    })
+export const _Nothing = () => new Nothing();
