79b5ae91112580735c892541845928df93eeb5bc
Anton Ovechkin <owanturist@gmail.com>
2017-04-16 14:03:50 +0700

    Added another functional interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,44 @@
-export interface Maybe<T> {
-    of<U>(a: U): Maybe<U>,
-    map<U>(f: (a: T) => U): Maybe<U>,
-    ap<U>(b: Maybe<(a: T) => U>): Maybe<U>,
-    chain<U>(f: (a: T) => Maybe<U>): Maybe<U>,
-    orElse(f: () => Maybe<T>): Maybe<T>,
-    getOrElse(defaults: T): T,
-    cata<U>(pattern: MaybePattern<T, U>): U
+export interface Functor<T> {
+    map<R>(f: (a: T) => R): Functor<R>;
+}
+
+export interface Apply<T> extends Functor<T> {
+    map<R>(f: (a: T) => R): Apply<R>;
+    of<R>(a: R): Apply<R>;
+}
+
+export interface Applicative<T> extends Apply<T> {
+    map<R>(f: (a: T) => R): Applicative<R>;
+    of<R>(a: R): Applicative<R>;
+    ap<R>(f: Applicative<(a: T) => R>): Applicative<R>;
+}
+
+export interface Chain<T> extends Apply<T> {
+    map<R>(f: (a: T) => R): Chain<R>;
+    of<R>(a: R): Chain<R>;
+    chain<R>(f: (a: T) => Chain<R>): Chain<R>;
+}
+
+export interface Monad<T> extends Applicative<T>, Chain<T> {
+    map<R>(f: (a: T) => R): Monad<R>;
+    of<R>(a: R): Monad<R>;
+    ap<R>(f: Monad<(a: T) => R>): Monad<R>;
+    chain<R>(f: (a: T) => Monad<R>): Monad<R>;
+}
+
+export interface Maybe<T> extends Monad<T> {
+    of<U>(a: U): Maybe<U>;
+    map<U>(f: (a: T) => U): Maybe<U>;
+    ap<U>(b: Maybe<(a: T) => U>): Maybe<U>;
+    chain<U>(f: (a: T) => Maybe<U>): Maybe<U>;
+    orElse(f: () => Maybe<T>): Maybe<T>;
+    getOrElse(defaults: T): T;
+    cata<U>(pattern: MaybePattern<T, U>): U;
 }
 
 export interface MaybePattern<T, U> {
-    Nothing(): U,
-    Just(a: T): U
+    Nothing(): U;
+    Just(a: T): U;
 }
 
 class Just<T> implements Maybe<T> {
