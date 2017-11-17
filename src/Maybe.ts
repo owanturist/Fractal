3a4391afa8eacc852891306c4b3807b9caa744e9
Anton Ovechkin <owanturist@gmail.com>
2017-11-17 19:47:04 +0700

    Implemented by elm way

--- src/Maybe.ts
+++ src/Maybe.ts
@@ -1,6 +1,74 @@
-abstract class Maybe<T> {
+export abstract class Maybe<T> {
     public static map<T, R>(fn: (value: T) => R, maybe: Maybe<T>): Maybe<R> {
         return maybe.map(fn);
+    }
+
+    public static map2<T1, T2, R>(
+        fn: (t1: T1, t2: T2) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>
+    ): Maybe<R> {
+        return Maybe.andThen(
+            t1 => Maybe.map(
+                t2 => fn(t1, t2),
+                m2
+            ),
+            m1
+        );
+    }
+
+    public static map3<T1, T2, T3, R>(
+        fn: (t1: T1, t2: T2, t3: T3) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>,
+        m3: Maybe<T3>
+    ): Maybe<R> {
+        return Maybe.andThen(
+            t1 => Maybe.map2(
+                (t2, t3) => fn(t1, t2, t3),
+                m2,
+                m3
+            ),
+            m1
+        );
+    }
+
+    public static map4<T1, T2, T3, T4, R>(
+        fn: (t1: T1, t2: T2, t3: T3, t4: T4) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>,
+        m3: Maybe<T3>,
+        m4: Maybe<T4>
+    ): Maybe<R> {
+        return Maybe.andThen(
+            t1 => Maybe.map3(
+                (t2, t3, t4) => fn(t1, t2, t3, t4),
+                m2,
+                m3,
+                m4
+            ),
+            m1
+        );
+    }
+
+    public static map5<T1, T2, T3, T4, T5, R>(
+        fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>,
+        m3: Maybe<T3>,
+        m4: Maybe<T4>,
+        m5: Maybe<T5>
+    ): Maybe<R> {
+        return Maybe.andThen(
+            t1 => Maybe.map4(
+                (t2, t3, t4, t5) => fn(t1, t2, t3, t4, t5),
+                m2,
+                m3,
+                m4,
+                m5
+            ),
+            m1
+        );
     }
 
     public static andThen<T, R>(fn: (value: T) => Maybe<R>, maybe: Maybe<T>): Maybe<R> {
@@ -29,57 +97,50 @@ interface Pattern<T, R> {
     Just(value: T): R;
 }
 
-class Just_<T> extends Maybe<T> {
-    constructor(private readonly value: T) {
-        super();
+const maybe = {
+    Just: class Just<T> extends Maybe<T> {
+        constructor(private readonly value: T) {
+            super();
+        }
+
+        protected map<R>(fn: (value: T) => R): Maybe<R> {
+            return new Just(
+                fn(this.value)
+            );
+        }
+
+        protected andThen<R>(fn: (value: T) => Maybe<R>): Maybe<R> {
+            return fn(this.value);
+        }
+
+        protected withDefault(): T {
+            return this.value;
+        }
+
+        protected cata<R>(pattern: Pattern<T, R>): R {
+            return pattern.Just(this.value);
+        }
+    },
+
+    Nothing: class Nothing extends Maybe<any> {
+        protected map(): Maybe<any> {
+            return this;
+        }
+
+        protected andThen(): Maybe<any> {
+            return this;
+        }
+
+        protected withDefault<T>(defaults: T): T {
+            return defaults;
+        }
+
+        protected cata<R>(pattern: Pattern<any, R>): R {
+            return pattern.Nothing();
+        }
     }
+};
 
-    protected map<R>(fn: (value: T) => R): Maybe<R> {
-        return new Just_(
-            fn(this.value)
-        );
-    }
+export const Just = <T>(value: T): Maybe<T> => new maybe.Just(value);
 
-    protected andThen<R>(fn: (value: T) => Maybe<R>): Maybe<R> {
-        return fn(this.value);
-    }
-
-    protected withDefault(): T {
-        return this.value;
-    }
-
-    protected cata<R>(pattern: Pattern<T, R>): R {
-        return pattern.Just(this.value);
-    }
-}
-
-class Nothing_ extends Maybe<any> {
-    protected map(): Maybe<any> {
-        return this;
-    }
-
-    protected andThen(): Maybe<any> {
-        return this;
-    }
-
-    protected withDefault<T>(defaults: T): T {
-        return defaults;
-    }
-
-    protected cata<R>(pattern: Pattern<any, R>): R {
-        return pattern.Nothing();
-    }
-}
-
-const Just = <T>(value: T): Maybe<T> => new Just_(value);
-
-const Nothing: Maybe<any> = new Nothing_();
-
-const map = Maybe.map;
-
-const andThen = Maybe.andThen;
-
-const withDefault = Maybe.withDefault;
-
-const cata = Maybe.cata;
-
+export const Nothing: Maybe<any> = new maybe.Nothing();
