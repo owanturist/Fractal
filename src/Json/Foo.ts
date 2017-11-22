81fa76246846e148ab619759e6ff6dd2f938c631
Anton Ovechkin <owanturist@gmail.com>
2017-11-22 12:33:30 +0700

    OOP based string|bool|number|list|fail|succeed|map|andThen were implemented

---
+++ src/Json/Foo.ts
@@ -0,0 +1,139 @@
+import {
+    Result,
+    Err,
+    Ok
+} from '../Result';
+
+export abstract class Decoder<T> {
+    protected abstract decode(input: any): Result<string, T>;
+
+    public static decodeValue<T>(decoder: Decoder<T>, input: any): Result<string, T> {
+        return decoder.decode(input);
+    }
+}
+
+export const decodeValue = Decoder.decodeValue;
+
+class Primitive<T> extends Decoder<T> {
+    constructor(
+        private readonly title: string,
+        private readonly check: (input: any) => input is T
+    ) {
+        super();
+    }
+
+    protected decode(input: any): Result<string, T> {
+        return this.check(input)
+            ? Ok(input)
+            : Err('Value `' + JSON.stringify(input) + '` is not a ' + this.title + '.')
+    }
+}
+
+class List<T> extends Decoder<Array<T>> {
+    constructor(private readonly decoder: Decoder<T>) {
+        super();
+    }
+
+    protected decode(input: any): Result<string, Array<T>> {
+        if (!(input instanceof Array)) {
+            return Err('Value `' + JSON.stringify(input) + '` is not an array.');
+        }
+
+        let acc: Result<string, Array<T>> = Ok([]);
+
+        for (const item of input) {
+            acc = Result.andThen(
+                accResult => Result.map(
+                    itemResult => {
+                        accResult.push(itemResult);
+
+                        return accResult;
+                    },
+                    decodeValue(this.decoder, item)
+                ),
+                acc
+            );
+        }
+
+        return acc;
+    }
+}
+
+class Fail extends Decoder<any> {
+    constructor(private readonly msg: string) {
+        super();
+    }
+
+    protected decode(): Result<string, any> {
+        return Err(this.msg);
+    }
+}
+
+class Succeed<T> extends Decoder<T> {
+    constructor(private readonly value: T) {
+        super();
+    }
+
+    protected decode(): Result<string, T> {
+        return Ok(this.value);
+    }
+}
+
+class Map<T, R> extends Decoder<R> {
+    constructor(
+        private readonly fn: (value: T) => R,
+        private readonly decoder: Decoder<T>
+    ) {
+        super();
+    }
+
+    protected decode(input: any): Result<string, R> {
+        return Result.map(
+            this.fn,
+            decodeValue(this.decoder, input)
+        );
+    }
+}
+
+class AndThen <T, R> extends Decoder<R> {
+    constructor(
+        private readonly fn: (value: T) => Decoder<R>,
+        private readonly decoder: Decoder<T>
+    ) {
+        super();
+    }
+
+    protected decode(input: any): Result<string, R> {
+        return Result.andThen(
+            value => decodeValue(this.fn(value), input),
+            decodeValue(this.decoder, input)
+        );
+    }
+}
+
+export const Decode = {
+    string: new Primitive(
+        'string',
+        (value: any): value is string => typeof value === 'string'
+    ) as Decoder<string>,
+
+    number: new Primitive(
+        'number',
+        (value: any): value is number => typeof value === 'number'
+    ) as Decoder<number>,
+
+    bool: new Primitive(
+        'bool',
+        (value: any): value is boolean => typeof value === 'boolean'
+    ) as Decoder<boolean>,
+
+    list: <T>(decoder: Decoder<T>): Decoder<Array<T>> => new List(decoder),
+
+    fail: (msg: string): Decoder<any> => new Fail(msg),
+
+    succeed: <T>(value: T): Decoder<T> => new Succeed(value),
+
+    map: <T, R>(fn: (value: T) => R, decoder: Decoder<T>): Decoder<R> => new Map(fn, decoder),
+
+    andThen: <T, R>(fn: (value: T) => Decoder<R>, decoder: Decoder<T>): Decoder<R> => new AndThen(fn, decoder)
+};
---
+++ test/Json/Foo.test.ts
@@ -0,0 +1,143 @@
+import test from 'ava';
+
+import {
+    Err,
+    Ok
+} from '../../src/Result';
+import {
+    decodeValue,
+    Decode
+} from '../../src/Json/Foo';
+
+test('Json.Foo.string', t => {
+    t.deepEqual(
+        decodeValue(
+            Decode.string,
+            1
+        ),
+        Err('Value `1` is not a string.')
+    );
+
+    t.deepEqual(
+        decodeValue(
+            Decode.string,
+            'str'
+        ),
+        Ok('str')
+    );
+});
+
+test('Json.Foo.number', t => {
+    t.deepEqual(
+        decodeValue(
+            Decode.number,
+            'str'
+        ),
+        Err('Value `"str"` is not a number.')
+    );
+
+    t.deepEqual(
+        decodeValue(
+            Decode.number,
+            1
+        ),
+        Ok(1)
+    );
+});
+
+test('Json.Foo.bool', t => {
+    t.deepEqual(
+        decodeValue(
+            Decode.bool,
+            1
+        ),
+        Err('Value `1` is not a bool.')
+    );
+
+    t.deepEqual(
+        decodeValue(
+            Decode.bool,
+            false
+        ),
+        Ok(false)
+    );
+});
+
+test('Json.Decode.list', t => {
+    const decoder = Decode.list(Decode.string);
+
+    t.deepEqual(
+        decodeValue(decoder, {}),
+        Err('Value `{}` is not an array.')
+    );
+
+    t.deepEqual(
+        decodeValue(decoder, [ 1, 2 ]),
+        Err('Value `1` is not a string.')
+    );
+
+    t.deepEqual(
+        decodeValue(decoder, [ 'str1', 2 ]),
+        Err('Value `2` is not a string.')
+    );
+
+    t.deepEqual(
+        decodeValue(decoder, [ 'str1', 'str2' ]),
+        Ok([ 'str1', 'str2' ])
+    );
+});
+
+test('Json.Decode.fail', t => {
+    t.deepEqual(
+        decodeValue(Decode.fail('msg'), null),
+        Err('msg')
+    )
+});
+
+test('Json.Decode.succeed', t => {
+    t.deepEqual(
+        decodeValue(Decode.succeed(1), null),
+        Ok(1)
+    )
+});
+
+test('Json.Decode.map', t => {
+    const decoder = Decode.map(
+        t1 => ({ t1 }),
+        Decode.string
+    );
+
+    t.deepEqual(
+        decodeValue(decoder, 1),
+        Err('Value `1` is not a string.')
+    );
+
+    t.deepEqual(
+        decodeValue(decoder, 'str'),
+        Ok({
+            t1: 'str'
+        })
+    );
+});
+
+test('Json.Decode.andThen', t => {
+    const decoder = Decode.andThen(
+        t1 => t1 % 2 === 0 ? Decode.succeed(t1 - 1) : Decode.fail('msg'),
+        Decode.number
+    );
+
+    t.deepEqual(
+        decodeValue(decoder, 'str'),
+        Err('Value `"str"` is not a number.')
+    );
+
+    t.deepEqual(
+        decodeValue(decoder, 1),
+        Err('msg')
+    );
+
+    t.deepEqual(
+        decodeValue(decoder, 2),
+        Ok(1)
+    );
+});
