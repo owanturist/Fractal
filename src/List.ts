0742cebc96f22d24783ae2fad3bb8565e86ff7d0
Anton Ovechkin <owanturist@gmail.com>
2017-11-24 18:26:37 +0700

    Array based List.isEmpty|size|reverse|member were implemented

--- src/List.ts
+++ src/List.ts
@@ -1,161 +1,38 @@
-import {
-    Maybe,
-    Nothing,
-    Just
-} from './Maybe';
-
-export abstract class List<T> {
-    protected abstract isEmpty(): boolean;
-
-    protected abstract member(value: T): boolean;
-
-    protected abstract head(): Maybe<T>;
-
-    protected abstract tail(): Maybe<List<T>>;
-
-    protected abstract foldl<R>(fn: (value: T, acc: R) => R, acc: R): R;
-
-    protected abstract map<R>(fn: (value: T) => R): List<R>;
-
-    protected abstract cata<R>(pattern: Pattern<T, R>): R;
-
+export abstract class List<T> extends Array<T> {
     public static isEmpty<T>(list: List<T>): boolean {
-        return list.isEmpty();
-    }
-
-    public static member<T>(value: T, list: List<T>): boolean {
-        return list.member(value);
-    }
-
-    public static tail<T>(list: List<T>): Maybe<List<T>> {
-        return list.tail();
-    }
-
-    public static filter<T>(fn: (value: T) => boolean, list: List<T>): List<T> {
-        return this.reverse(
-            list.foldl(
-                (value, acc) => fn(value) ? Cons(value, acc) : acc,
-                Nil
-            )
-        );
-    }
-
-    public static head<T>(list: List<T>): Maybe<T> {
-        return list.head();
+        return list.length === 0;
     }
 
     public static size<T>(list: List<T>): number {
-        return this.foldl(
-            (_, acc) => acc + 1,
-            0,
-            list
-        );
+        return list.length;
     }
 
     public static reverse<T>(list: List<T>): List<T> {
-        return this.foldl(
-            (value, acc) => Cons(value, acc),
-            Nil,
-            list
-        );
-    }
+        switch (list.length) {
+            case 0:
+            case 1: {
+                return list;
+            }
 
-    public static foldl<T, R>(fn: (value: T, acc: R) => R, acc: R, list: List<T>): R {
-        return list.foldl(fn, acc);
-    }
+            default: {
+                const res: List<T> = [];
 
-    public static foldr<T, R>(fn: (value: T, acc: R) => R, acc: R, list: List<T>): R {
-        return this.reverse(list).foldl(fn, acc);
-    }
+                for (let i = list.length - 1; i >= 0; i--) {
+                    res.push(list[ i ]);
+                }
 
-    public static map<T, R>(fn: (value: T) => R, list: List<T>): List<R> {
-        return list.map(fn);
-    }
-
-    public static cata<T, R>(list: List<T>, pattern: Pattern<T, R>): R {
-        return list.cata(pattern);
-    }
-}
-
-interface Pattern<T, R> {
-    Nil: () => R,
-    Cons: (value: T, next: List<T>) => R
-}
-
-const list = {
-    Nil: class Nil<T> extends List<T> {
-        protected isEmpty() {
-            return true;
-        }
-
-        protected member(): boolean {
-            return false;
-        }
-
-        protected head(): Maybe<T> {
-            return Nothing;
-        }
-
-        protected tail(): Maybe<List<T>> {
-            return Nothing;
-        }
-
-        protected foldl<R>(_fn: (value: T, acc: R) => R, acc: R): R {
-            return acc;
-        }
-
-        protected map<R>(): List<R> {
-            return new Nil();
-        }
-
-        protected cata<R>(pattern: Pattern<T, R>): R {
-            return pattern.Nil();
-        }
-    },
-    Cons: class Cons<T> extends List<T> {
-        constructor (private readonly value: T, private readonly next: List<T>) {
-            super();
-        }
-
-        protected isEmpty() {
-            return false;
-        }
-
-        protected member(value: T): boolean {
-            return value === this.value;
-        }
-
-        protected head(): Maybe<T> {
-            return Just(this.value);
-        }
-
-        protected tail(): Maybe<List<T>> {
-            return Just(this.next);
-        }
-
-        protected foldl<R>(fn: (value: T, acc: R) => R, acc: R): R {
-            return List.foldl(
-                fn,
-                fn(this.value, acc),
-                this.next
-            );
-        }
-
-        protected map<R>(fn: (value: T) => R): List<R> {
-            return new Cons(
-                fn(this.value),
-                List.map(fn, this.next)
-            );
-        }
-
-        protected cata<R>(pattern: Pattern<T, R>): R {
-            return pattern.Cons(this.value, this.next);
+                return res;
+            }
         }
     }
-};
 
-export const Nil: List<any> = new list.Nil();
+    public static member<T>(value: T, list: List<T>): boolean {
+        for (const item of list) {
+            if (value === item) {
+                return true;
+            }
+        }
 
-export function Cons<T>(value: T, next: List<T>): List<T> {
-    return new list.Cons(value, next);
+        return false;
+    }
 }
--- test/List.test.ts
+++ test/List.test.ts
@@ -1,12 +1,87 @@
 import test from 'ava';
 
-test.todo('List.isEmpty');
+import {
+    List
+} from '../src/List';
 
-test.todo('List.size');
+test('List.isEmpty', t => {
+    t.true(
+        List.isEmpty([])
+    );
 
-test.todo('List.reverse');
+    t.false(
+        List.isEmpty([ 1, 2, 3 ])
+    );
+});
 
-test.todo('List.member');
+test('List.size', t => {
+    t.is(
+        List.size([]),
+        0
+    );
+
+    t.is(
+        List.size([ 1, 2, 3 ]),
+        3
+    );
+});
+
+test('List.reverse', t => {
+    const list1: List<number> = [];
+
+    t.is(
+        List.reverse(list1),
+        list1
+    );
+
+    t.deepEqual(
+        List.reverse(list1),
+        []
+    );
+
+    const list2 = [ 1 ];
+
+    t.is(
+        List.reverse(list2),
+        list2
+    );
+
+    t.deepEqual(
+        List.reverse(list2),
+        [ 1 ]
+    );
+
+    const list3 = [ 1, 2, 3 ];
+
+    t.not(
+        List.reverse(list3),
+        list3
+    );
+
+    t.deepEqual(
+        List.reverse(list3),
+        [ 3, 2, 1 ]
+    );
+
+    t.deepEqual(
+        list3,
+        [ 1, 2, 3 ]
+    );
+});
+
+test('List.member', t => {
+    t.false(
+        List.member(0, []),
+    );
+
+    t.false(
+        List.member(0, [ 1, 2, 3 ]),
+    );
+
+    t.true(
+        List.member(0, [ 0, 1, 2, 3 ]),
+    );
+});
 
 test.todo('List.head');
 
