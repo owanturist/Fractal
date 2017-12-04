1b8ca7f8dffa0099782c1119176c6930e91ccd40
Anton Ovechkin <owanturist@gmail.com>
2017-12-04 13:58:13 +0700

    Isolated Decode statics methods

--- src/Json/Decode.ts
+++ src/Json/Decode.ts
@@ -14,21 +14,7 @@ import {
 
 export abstract class Decoder<T> {
     protected abstract decode(input: any): Result<string, T>;
-
-    public static decode<T>(decoder: Decoder<T>, input: any): Result<string, T> {
-        return decoder.decode(input);
-    }
 }
-
-export const decodeValue = Decoder.decode;
-
-export const decodeString = <T>(decoder: Decoder<T>, str: string): Result<string, T> => {
-    try {
-        return decodeValue(decoder, JSON.parse(str));
-    } catch (err) {
-        return Err(err.message);
-    }
-};
 
 class Primitive<T> extends Decoder<T> {
     constructor(
@@ -45,21 +31,6 @@ class Primitive<T> extends Decoder<T> {
     }
 }
 
-export const string: Decoder<string> = new Primitive(
-    'string',
-    (value: any): value is string => typeof value === 'string'
-);
-
-export const number: Decoder<number> = new Primitive(
-    'number',
-    (value: any): value is number => typeof value === 'number'
-);
-
-export const bool: Decoder<boolean> = new Primitive(
-    'bool',
-    (value: any): value is boolean => typeof value === 'boolean'
-);
-
 class Nullable<T> extends Decoder<Maybe_<T>> {
     constructor(private readonly decoder: Decoder<T>) {
         super();
@@ -70,10 +41,6 @@ class Nullable<T> extends Decoder<Maybe_<T>> {
             ? Ok(Nothing)
             : Result.map(Just, decodeValue(this.decoder, input))
     }
-}
-
-export function nullable<T>(decoder: Decoder<T>): Decoder<Maybe_<T>> {
-    return new Nullable(decoder);
 }
 
 class List<T> extends Decoder<Array<T>> {
@@ -104,10 +71,6 @@ class List<T> extends Decoder<Array<T>> {
 
         return acc;
     }
-}
-
-export function list<T>(decoder: Decoder<T>): Decoder<Array<T>> {
-    return new List(decoder);
 }
 
 class Dict<T> extends Decoder<{[ key: string ]: T}> {
@@ -142,10 +105,6 @@ class Dict<T> extends Decoder<{[ key: string ]: T}> {
     }
 }
 
-export function dict<T>(decoder: Decoder<T>): Decoder<{[ key: string ]: T}> {
-    return new Dict(decoder);
-}
-
 class KeyValuePairs<T> extends Decoder<Array<[ string, T ]>> {
     constructor(private readonly decoder: Decoder<T>) {
         super();
@@ -178,10 +137,6 @@ class KeyValuePairs<T> extends Decoder<Array<[ string, T ]>> {
     }
 }
 
-export function keyValuePairs<T>(decoder: Decoder<T>): Decoder<Array<[ string, T ]>> {
-    return new KeyValuePairs(decoder)
-}
-
 class Field<T> extends Decoder<T> {
     constructor(
         private readonly key: string,
@@ -199,20 +154,6 @@ class Field<T> extends Decoder<T> {
             ? decodeValue(this.decoder, input[ this.key ])
             : Err('Field `' + this.key + '` doesn\'t exist in an object ' + JSON.stringify(input) + '.');
     }
-}
-
-export function field<T>(key: string, decoder: Decoder<T>): Decoder<T> {
-    return new Field(key, decoder);
-}
-
-export function at<T>(keys: Array<string>, decoder: Decoder<T>): Decoder<T> {
-    let acc = decoder;
-
-    for (let i = keys.length - 1; i >= 0; i--) {
-        acc = new Field(keys[ i ], acc);
-    }
-
-    return acc;
 }
 
 class Index<T> extends Decoder<T> {
@@ -234,10 +175,6 @@ class Index<T> extends Decoder<T> {
     }
 }
 
-export function index<T>(index: number, decoder: Decoder<T>): Decoder<T> {
-    return new Index(index, decoder);
-}
-
 class Maybe<T> extends Decoder<Maybe_<T>> {
     constructor(private readonly decoder: Decoder<T>) {
         super();
@@ -250,10 +187,6 @@ class Maybe<T> extends Decoder<Maybe_<T>> {
             )
         );
     }
-}
-
-export function maybe<T>(decoder: Decoder<T>): Decoder<Maybe_<T>> {
-    return new Maybe(decoder);
 }
 
 class OneOf<T> extends Decoder<T> {
@@ -283,10 +216,6 @@ class OneOf<T> extends Decoder<T> {
     }
 }
 
-export function oneOf<T>(decoders: Array<Decoder<T>>): Decoder<T> {
-    return new OneOf(decoders);
-}
-
 class Lazy<T> extends Decoder<T> {
     constructor(private readonly callDecoder: () => Decoder<T>) {
         super();
@@ -297,17 +226,11 @@ class Lazy<T> extends Decoder<T> {
     }
 }
 
-export function lazy<T>(callDecoder: () => Decoder<T>): Decoder<T> {
-    return new Lazy(callDecoder);
-}
-
 class Value extends Decoder<any> {
     protected decode(input: any): Result<string, Value_> {
         return Ok(new Value_(input));
     }
 }
-
-export const value = new Value();
 
 class Nul<T> extends Decoder<T> {
     constructor(private readonly defaults: T) {
@@ -321,10 +244,6 @@ class Nul<T> extends Decoder<T> {
     }
 }
 
-export function nul<T>(defaults: T): Decoder<T> {
-    return new Nul(defaults);
-}
-
 class Fail extends Decoder<any> {
     constructor(private readonly msg: string) {
         super();
@@ -335,10 +254,6 @@ class Fail extends Decoder<any> {
     }
 }
 
-export function fail(msg: string): Decoder<any> {
-    return new Fail(msg);
-}
-
 class Succeed<T> extends Decoder<T> {
     constructor(private readonly value: T) {
         super();
@@ -347,10 +262,6 @@ class Succeed<T> extends Decoder<T> {
     protected decode(): Result<string, T> {
         return Ok(this.value);
     }
-}
-
-export function succeed<T>(value: T): Decoder<T> {
-    return new Succeed(value);
 }
 
 class Map<T, R> extends Decoder<R> {
@@ -369,151 +280,7 @@ class Map<T, R> extends Decoder<R> {
     }
 }
 
-export function map<T, R>(fn: (value: T) => R, decoder: Decoder<T>): Decoder<R> {
-    return new Map(fn, decoder);
-}
-
-export function map2<T1, T2, R>(
-        fn: (t1: T1, t2: T2) => R,
-        d1: Decoder<T1>,
-        d2: Decoder<T2>
-    ): Decoder<R> {
-    return andThen(
-        t1 => map(
-            t2 => fn(t1, t2),
-            d2
-        ),
-        d1
-    );
-}
-
-export function map3<T1, T2, T3, R>(
-        fn: (t1: T1, t2: T2, t3: T3) => R,
-        d1: Decoder<T1>,
-        d2: Decoder<T2>,
-        d3: Decoder<T3>
-    ): Decoder<R> {
-    return andThen(
-        t1 => map2(
-            (t2, t3) => fn(t1, t2, t3),
-            d2,
-            d3
-        ),
-        d1
-    );
-}
-
-export function map4<T1, T2, T3, T4, R>(
-        fn: (t1: T1, t2: T2, t3: T3, t4: T4) => R,
-        d1: Decoder<T1>,
-        d2: Decoder<T2>,
-        d3: Decoder<T3>,
-        d4: Decoder<T4>
-    ): Decoder<R> {
-    return andThen(
-        t1 => map3(
-            (t2, t3, t4) => fn(t1, t2, t3, t4),
-            d2,
-            d3,
-            d4
-        ),
-        d1
-    );
-}
-
-export function map5<T1, T2, T3, T4, T5, R>(
-        fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) => R,
-        d1: Decoder<T1>,
-        d2: Decoder<T2>,
-        d3: Decoder<T3>,
-        d4: Decoder<T4>,
-        d5: Decoder<T5>
-    ): Decoder<R> {
-    return andThen(
-        t1 => map4(
-            (t2, t3, t4, t5) => fn(t1, t2, t3, t4, t5),
-            d2,
-            d3,
-            d4,
-            d5
-        ),
-        d1
-    );
-}
-
-export function map6<T1, T2, T3, T4, T5, T6, R>(
-        fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6) => R,
-        d1: Decoder<T1>,
-        d2: Decoder<T2>,
-        d3: Decoder<T3>,
-        d4: Decoder<T4>,
-        d5: Decoder<T5>,
-        d6: Decoder<T6>
-    ): Decoder<R> {
-    return andThen(
-        t1 => map5(
-            (t2, t3, t4, t5, t6) => fn(t1, t2, t3, t4, t5, t6),
-            d2,
-            d3,
-            d4,
-            d5,
-            d6
-        ),
-        d1
-    );
-}
-
-export function map7<T1, T2, T3, T4, T5, T6, T7, R>(
-        fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7) => R,
-        d1: Decoder<T1>,
-        d2: Decoder<T2>,
-        d3: Decoder<T3>,
-        d4: Decoder<T4>,
-        d5: Decoder<T5>,
-        d6: Decoder<T6>,
-        d7: Decoder<T7>
-    ): Decoder<R> {
-    return andThen(
-        t1 => map6(
-            (t2, t3, t4, t5, t6, t7) => fn(t1, t2, t3, t4, t5, t6, t7),
-            d2,
-            d3,
-            d4,
-            d5,
-            d6,
-            d7
-        ),
-        d1
-    );
-}
-
-export function map8<T1, T2, T3, T4, T5, T6, T7, T8, R>(
-        fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8) => R,
-        d1: Decoder<T1>,
-        d2: Decoder<T2>,
-        d3: Decoder<T3>,
-        d4: Decoder<T4>,
-        d5: Decoder<T5>,
-        d6: Decoder<T6>,
-        d7: Decoder<T7>,
-        d8: Decoder<T8>
-    ): Decoder<R> {
-    return andThen(
-        t1 => map7(
-            (t2, t3, t4, t5, t6, t7, t8) => fn(t1, t2, t3, t4, t5, t6, t7, t8),
-            d2,
-            d3,
-            d4,
-            d5,
-            d6,
-            d7,
-            d8
-        ),
-        d1
-    );
-}
-
-class AndThen <T, R> extends Decoder<R> {
+class AndThen<T, R> extends Decoder<R> {
     constructor(
         private readonly fn: (value: T) => Decoder<R>,
         private readonly decoder: Decoder<T>
@@ -529,37 +296,243 @@ class AndThen <T, R> extends Decoder<R> {
     }
 }
 
-export function andThen<T, R>(fn: (value: T) => Decoder<R>, decoder: Decoder<T>): Decoder<R> {
-    return new AndThen(fn, decoder);
+export abstract class Decode<T> extends Decoder<T> {
+    public static string: Decoder<string> = new Primitive(
+        'string',
+        (value: any): value is string => typeof value === 'string'
+    );
+
+    public static number: Decoder<number> = new Primitive(
+        'number',
+        (value: any): value is number => typeof value === 'number'
+    );
+
+    public static bool: Decoder<boolean> = new Primitive(
+        'bool',
+        (value: any): value is boolean => typeof value === 'boolean'
+    );
+
+    public static nullable<T>(decoder: Decoder<T>): Decoder<Maybe_<T>> {
+        return new Nullable(decoder);
+    }
+
+    public static list<T>(decoder: Decoder<T>): Decoder<Array<T>> {
+        return new List(decoder);
+    }
+
+    public static dict<T>(decoder: Decoder<T>): Decoder<{[ key: string ]: T}> {
+        return new Dict(decoder);
+    }
+
+    public static keyValuePairs<T>(decoder: Decoder<T>): Decoder<Array<[ string, T ]>> {
+        return new KeyValuePairs(decoder)
+    }
+
+    public static field<T>(key: string, decoder: Decoder<T>): Decoder<T> {
+        return new Field(key, decoder);
+    }
+
+    public static at<T>(keys: Array<string>, decoder: Decoder<T>): Decoder<T> {
+        let acc = decoder;
+
+        for (let i = keys.length - 1; i >= 0; i--) {
+            acc = new Field(keys[ i ], acc);
+        }
+
+        return acc;
+    }
+
+    public static index<T>(index: number, decoder: Decoder<T>): Decoder<T> {
+        return new Index(index, decoder);
+    }
+
+    public static maybe<T>(decoder: Decoder<T>): Decoder<Maybe_<T>> {
+        return new Maybe(decoder);
+    }
+
+    public static oneOf<T>(decoders: Array<Decoder<T>>): Decoder<T> {
+        return new OneOf(decoders);
+    }
+
+    public static lazy<T>(callDecoder: () => Decoder<T>): Decoder<T> {
+        return new Lazy(callDecoder);
+    }
+
+    public static value = new Value();
+
+    public static nul<T>(defaults: T): Decoder<T> {
+        return new Nul(defaults);
+    }
+
+    public static fail(msg: string): Decoder<any> {
+        return new Fail(msg);
+    }
+
+    public static succeed<T>(value: T): Decoder<T> {
+        return new Succeed(value);
+    }
+
+    public static map<T, R>(fn: (value: T) => R, decoder: Decoder<T>): Decoder<R> {
+        return new Map(fn, decoder);
+    }
+
+    public static map2<T1, T2, R>(
+            fn: (t1: T1, t2: T2) => R,
+            d1: Decoder<T1>,
+            d2: Decoder<T2>
+        ): Decoder<R> {
+        return this.andThen(
+            t1 => this.map(
+                t2 => fn(t1, t2),
+                d2
+            ),
+            d1
+        );
+    }
+
+    public static map3<T1, T2, T3, R>(
+            fn: (t1: T1, t2: T2, t3: T3) => R,
+            d1: Decoder<T1>,
+            d2: Decoder<T2>,
+            d3: Decoder<T3>
+        ): Decoder<R> {
+        return this.andThen(
+            t1 => this.map2(
+                (t2, t3) => fn(t1, t2, t3),
+                d2,
+                d3
+            ),
+            d1
+        );
+    }
+
+    public static map4<T1, T2, T3, T4, R>(
+            fn: (t1: T1, t2: T2, t3: T3, t4: T4) => R,
+            d1: Decoder<T1>,
+            d2: Decoder<T2>,
+            d3: Decoder<T3>,
+            d4: Decoder<T4>
+        ): Decoder<R> {
+        return this.andThen(
+            t1 => this.map3(
+                (t2, t3, t4) => fn(t1, t2, t3, t4),
+                d2,
+                d3,
+                d4
+            ),
+            d1
+        );
+    }
+
+    public static map5<T1, T2, T3, T4, T5, R>(
+            fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) => R,
+            d1: Decoder<T1>,
+            d2: Decoder<T2>,
+            d3: Decoder<T3>,
+            d4: Decoder<T4>,
+            d5: Decoder<T5>
+        ): Decoder<R> {
+        return this.andThen(
+            t1 => this.map4(
+                (t2, t3, t4, t5) => fn(t1, t2, t3, t4, t5),
+                d2,
+                d3,
+                d4,
+                d5
+            ),
+            d1
+        );
+    }
+
+    public static map6<T1, T2, T3, T4, T5, T6, R>(
+            fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6) => R,
+            d1: Decoder<T1>,
+            d2: Decoder<T2>,
+            d3: Decoder<T3>,
+            d4: Decoder<T4>,
+            d5: Decoder<T5>,
+            d6: Decoder<T6>
+        ): Decoder<R> {
+        return this.andThen(
+            t1 => this.map5(
+                (t2, t3, t4, t5, t6) => fn(t1, t2, t3, t4, t5, t6),
+                d2,
+                d3,
+                d4,
+                d5,
+                d6
+            ),
+            d1
+        );
+    }
+
+    public static map7<T1, T2, T3, T4, T5, T6, T7, R>(
+            fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7) => R,
+            d1: Decoder<T1>,
+            d2: Decoder<T2>,
+            d3: Decoder<T3>,
+            d4: Decoder<T4>,
+            d5: Decoder<T5>,
+            d6: Decoder<T6>,
+            d7: Decoder<T7>
+        ): Decoder<R> {
+        return this.andThen(
+            t1 => this.map6(
+                (t2, t3, t4, t5, t6, t7) => fn(t1, t2, t3, t4, t5, t6, t7),
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7
+            ),
+            d1
+        );
+    }
+
+    public static map8<T1, T2, T3, T4, T5, T6, T7, T8, R>(
+            fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8) => R,
+            d1: Decoder<T1>,
+            d2: Decoder<T2>,
+            d3: Decoder<T3>,
+            d4: Decoder<T4>,
+            d5: Decoder<T5>,
+            d6: Decoder<T6>,
+            d7: Decoder<T7>,
+            d8: Decoder<T8>
+        ): Decoder<R> {
+        return this.andThen(
+            t1 => this.map7(
+                (t2, t3, t4, t5, t6, t7, t8) => fn(t1, t2, t3, t4, t5, t6, t7, t8),
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8
+            ),
+            d1
+        );
+    }
+
+    public static andThen<T, R>(fn: (value: T) => Decoder<R>, decoder: Decoder<T>): Decoder<R> {
+        return new AndThen(fn, decoder);
+    }
+
+    public static decodeValue<T>(decoder: Decoder<T>, input: any): Result<string, T> {
+        return (decoder as Decode<T>).decode(input);
+    }
+
+    public static decodeString<T>(decoder: Decoder<T>, str: string): Result<string, T> {
+        try {
+            return decodeValue(decoder, JSON.parse(str));
+        } catch (err) {
+            return Err(err.message);
+        }
+    }
 }
 
-export const Decode = {
-    string,
-    number,
-    bool,
+export const decodeValue = Decode.decodeValue;
 
-    nullable,
-    list,
-    dict,
-    keyValuePairs,
-    field,
-    at,
-    index,
-    maybe,
-    oneOf,
-    lazy,
-    value,
-    nul,
-    fail,
-    succeed,
-
-    map,
-    map2,
-    map3,
-    map4,
-    map5,
-    map6,
-    map7,
-    map8,
-    andThen
-};
+export const decodeString = Decode.decodeString;
