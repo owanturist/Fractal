c5d8efdc604d8fcc004c023c8444fa3fedc1bd55
Anton Ovechkin <owanturist@gmail.com>
2018-01-06 14:37:24 +0700

    Added static m2-m8 for OOP Maybe

--- src/Foo.ts
+++ src/Foo.ts
@@ -1,7 +1,7 @@
-interface Pattern<T, R> {
+type Pattern<T, R> = {
     Nothing: () => R;
     Just: (value: T) => R;
-}
+};
 
 export abstract class Maybe<T> {
 
@@ -23,11 +23,144 @@ export abstract class Maybe<T> {
 
     // TRANSFORMING
 
-    public abstract ap<R>(maybe: Maybe<(value: T) => R>): Maybe<R>;
     public abstract map<R>(fn: (value: T) => R): Maybe<R>;
     public abstract chain<R>(fn: (value: T) => Maybe<R>): Maybe<R>;
     public abstract orElse(fn: () => Maybe<T>): Maybe<T>;
     public abstract cata<R>(pattern: Pattern<T, R>): R;
+
+    // MAPPING
+
+    public static map2<T1, T2, R>(
+        fn: (t1: T1, t2: T2) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>
+    ): Maybe<R> {
+        return m1.chain(
+            t1 => m2.map(
+                t2 => fn(t1, t2)
+            )
+        );
+    }
+
+    public static map3<T1, T2, T3, R>(
+        fn: (t1: T1, t2: T2, t3: T3) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>,
+        m3: Maybe<T3>
+    ): Maybe<R> {
+        return m1.chain(
+            t1 => this.map2(
+                (t2, t3) => fn(t1, t2, t3),
+                m2,
+                m3
+            )
+        );
+    }
+
+    public static map4<T1, T2, T3, T4, R>(
+        fn: (t1: T1, t2: T2, t3: T3, t4: T4) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>,
+        m3: Maybe<T3>,
+        m4: Maybe<T4>
+    ): Maybe<R> {
+        return m1.chain(
+            t1 => this.map3(
+                (t2, t3, t4) => fn(t1, t2, t3, t4),
+                m2,
+                m3,
+                m4
+            )
+        );
+    }
+
+    public static map5<T1, T2, T3, T4, T5, R>(
+        fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>,
+        m3: Maybe<T3>,
+        m4: Maybe<T4>,
+        m5: Maybe<T5>
+    ): Maybe<R> {
+        return m1.chain(
+            t1 => this.map4(
+                (t2, t3, t4, t5) => fn(t1, t2, t3, t4, t5),
+                m2,
+                m3,
+                m4,
+                m5
+            )
+        );
+    }
+
+    public static map6<T1, T2, T3, T4, T5, T6, R>(
+        fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>,
+        m3: Maybe<T3>,
+        m4: Maybe<T4>,
+        m5: Maybe<T5>,
+        m6: Maybe<T6>
+    ): Maybe<R> {
+        return m1.chain(
+            t1 => this.map5(
+                (t2, t3, t4, t5, t6) => fn(t1, t2, t3, t4, t5, t6),
+                m2,
+                m3,
+                m4,
+                m5,
+                m6
+            )
+        );
+    }
+
+    public static map7<T1, T2, T3, T4, T5, T6, T7, R>(
+        fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>,
+        m3: Maybe<T3>,
+        m4: Maybe<T4>,
+        m5: Maybe<T5>,
+        m6: Maybe<T6>,
+        m7: Maybe<T7>
+    ): Maybe<R> {
+        return m1.chain(
+            t1 => this.map6(
+                (t2, t3, t4, t5, t6, t7) => fn(t1, t2, t3, t4, t5, t6, t7),
+                m2,
+                m3,
+                m4,
+                m5,
+                m6,
+                m7
+            )
+        );
+    }
+
+    public static map8<T1, T2, T3, T4, T5, T6, T7, T8, R>(
+        fn: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8) => R,
+        m1: Maybe<T1>,
+        m2: Maybe<T2>,
+        m3: Maybe<T3>,
+        m4: Maybe<T4>,
+        m5: Maybe<T5>,
+        m6: Maybe<T6>,
+        m7: Maybe<T7>,
+        m8: Maybe<T8>
+    ): Maybe<R> {
+        return m1.chain(
+            t1 => this.map7(
+                (t2, t3, t4, t5, t6, t7, t8) => fn(t1, t2, t3, t4, t5, t6, t7, t8),
+                m2,
+                m3,
+                m4,
+                m5,
+                m6,
+                m7,
+                m8
+            )
+        );
+    }
 }
 
 const maybe = {
@@ -50,10 +183,6 @@ const maybe = {
         }
 
         // TRANSFORMING
-
-        public ap<R>(): Maybe<R> {
-            return new Nothing();
-        }
 
         public map<R>(): Maybe<R> {
             return new Nothing();
@@ -94,10 +223,6 @@ const maybe = {
         }
 
         // TRANSFORMING
-
-        public ap<R>(maybe: Maybe<(value: T) => R>): Maybe<R> {
-            return maybe.chain(this.map)
-        }
 
         public map<R>(fn: (value: T) => R): Maybe<R> {
             return new Just(
