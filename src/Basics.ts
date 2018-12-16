3ce4f4f3bfd39545cff400af04d533d7e5a7a6b8
Anton Ovechkin <owanturist@gmail.com>
2018-12-16 20:16:12 +0700

    Basic.Curry has been invented

--- src/Basics.ts
+++ src/Basics.ts
@@ -13,3 +13,43 @@ export type Cata<T>
     ? T & { _?: never } | Combinations<T> & { _(): R }
     : T
     ;
+
+type Args<F>
+    = F extends (...args: infer A) => unknown
+    ? A
+    : never
+    ;
+
+type TailArgs<F>
+    = F extends (head: unknown, ...tail: infer A) => unknown
+    ? A
+    : F extends () => unknown ? [] : never
+    ;
+
+type Tail<T extends Array<unknown>> = TailArgs<(...args: T) => unknown>;
+
+type CastArray<T> = T extends Array<unknown> ? T : [];
+
+type Initial<T extends Array<unknown>, R extends Array<unknown> = Tail<T>> = CastArray<{
+    [ K in keyof R ]: T[ keyof T & K ];
+}>;
+
+type PotentialArgs<T extends Array<unknown>, R extends Array<unknown> = Initial<T>> = CastArray<{
+    next: R extends [] ? never : T | PotentialArgs<R, Initial<R>>;
+    end: R extends [] ? T : never;
+}[ R extends [] ? 'end' : 'next' ]>;
+
+type DropFromArraySize<T extends Array<unknown>, R extends Array<unknown>> = CastArray<{
+    next: R extends [] ? never : DropFromArraySize<Tail<T>, Tail<R>>;
+    end: R extends [] ? T : never;
+}[ R extends [] ? 'end' : 'next' ]>;
+
+type Curried<F extends (...args: Array<unknown>) => unknown> =
+    <
+        A extends Args<F>,
+        T extends PotentialArgs<A>,
+        R extends DropFromArraySize<A, T>
+    >(...args: T) => R extends [] ? ReturnType<F> : Curried<(...args: R) => ReturnType<F>>;
+
+export declare const curry: <F extends (...args: Array<unknown>) => unknown>(func: F) =>
+    Args<F> extends [] | [ unknown ] ? F : Curried<F>;
