73b37732d1ff9f93d0f4c37a43b8679ddec51c80
Anton Ovechkin <owanturist@gmail.com>
2019-02-21 22:57:56 +0700

    Fix errors

--- src/Elm/Scheduler.ts
+++ src/Elm/Scheduler.ts
@@ -1,195 +1,170 @@
-/*
+// T A S K S
 
-import Elm.Kernel.Utils exposing (Tuple0)
+const __1_SUCCEED = 'SUCCEED';
+const __1_FAIL = 'FAIL';
+const __1_BINDING = 'BINDING';
+const __1_AND_THEN = 'AND_THEN';
+const __1_ON_ERROR = 'ON_ERROR';
+const __1_RECEIVE = 'RECEIVE';
 
-*/
 
-
-// TASKS
-
-function _Scheduler_succeed(value)
-{
-	return {
-		$: __1_SUCCEED,
-		__value: value
-	};
+function _Scheduler_succeed(value) {
+    return {
+        $: __1_SUCCEED,
+        __value: value
+    };
 }
 
-function _Scheduler_fail(error)
-{
-	return {
-		$: __1_FAIL,
-		__value: error
-	};
+function _Scheduler_fail(error) {
+    return {
+        $: __1_FAIL,
+        __value: error
+    };
 }
 
-function _Scheduler_binding(callback)
-{
-	return {
-		$: __1_BINDING,
-		__callback: callback,
-		__kill: null
-	};
+function _Scheduler_binding(callback) {
+    return {
+        $: __1_BINDING,
+        __callback: callback,
+        __kill: null
+    };
 }
 
-var _Scheduler_andThen = F2(function(callback, task)
-{
-	return {
-		$: __1_AND_THEN,
-		__callback: callback,
-		__task: task
-	};
-});
+function _Scheduler_andThen(callback, task) {
+    return {
+        $: __1_AND_THEN,
+        __callback: callback,
+        __task: task
+    };
+}
 
-var _Scheduler_onError = F2(function(callback, task)
-{
-	return {
-		$: __1_ON_ERROR,
-		__callback: callback,
-		__task: task
-	};
-});
+function _Scheduler_onError(callback, task) {
+    return {
+        $: __1_ON_ERROR,
+        __callback: callback,
+        __task: task
+    };
+}
 
-function _Scheduler_receive(callback)
-{
-	return {
-		$: __1_RECEIVE,
-		__callback: callback
-	};
+function _Scheduler_receive(callback) {
+    return {
+        $: __1_RECEIVE,
+        __callback: callback
+    };
 }
 
 
-// PROCESSES
+// P R O C E S S E S
 
-var _Scheduler_guid = 0;
+const __2_PROCESS = 'PROCESS';
 
-function _Scheduler_rawSpawn(task)
-{
-	var proc = {
-		$: __2_PROCESS,
-		__id: _Scheduler_guid++,
-		__root: task,
-		__stack: null,
-		__mailbox: []
-	};
+let GUID = 0;
 
-	_Scheduler_enqueue(proc);
+function _Scheduler_rawSpawn(task) {
+    const proc = {
+        $: __2_PROCESS,
+        __id: GUID++,
+        __root: task,
+        __stack: null,
+        __mailbox: []
+    };
 
-	return proc;
+    _Scheduler_enqueue(proc);
+
+    return proc;
 }
 
-function _Scheduler_spawn(task)
-{
-	return _Scheduler_binding(function(callback) {
-		callback(_Scheduler_succeed(_Scheduler_rawSpawn(task)));
-	});
+function _Scheduler_spawn(task) {
+    return _Scheduler_binding(callback => {
+        callback(_Scheduler_succeed(_Scheduler_rawSpawn(task)));
+    });
 }
 
-function _Scheduler_rawSend(proc, msg)
-{
-	proc.__mailbox.push(msg);
-	_Scheduler_enqueue(proc);
+function _Scheduler_rawSend(proc, msg) {
+    proc.__mailbox.push(msg);
+    _Scheduler_enqueue(proc);
 }
 
-var _Scheduler_send = F2(function(proc, msg)
-{
-	return _Scheduler_binding(function(callback) {
-		_Scheduler_rawSend(proc, msg);
-		callback(_Scheduler_succeed(__Utils_Tuple0));
-	});
-});
+function _Scheduler_send(proc, msg) {
+    return _Scheduler_binding(callback => {
+        _Scheduler_rawSend(proc, msg);
+        callback(_Scheduler_succeed(null));
+    });
+}
 
-function _Scheduler_kill(proc)
-{
-	return _Scheduler_binding(function(callback) {
-		var task = proc.__root;
-		if (task.$ === __1_BINDING && task.__kill)
-		{
-			task.__kill();
-		}
+function _Scheduler_kill(proc) {
+    return _Scheduler_binding(callback => {
+        const task = proc.__root;
 
-		proc.__root = null;
+        if (task.$ === __1_BINDING && task.__kill) {
+            task.__kill();
+        }
 
-		callback(_Scheduler_succeed(__Utils_Tuple0));
-	});
+        proc.__root = null;
+
+        callback(_Scheduler_succeed(null));
+    });
 }
 
 
-/* STEP PROCESSES
+// S T E P
 
-type alias Process =
-  { $ : tag
-  , id : unique_id
-  , root : Task
-  , stack : null | { $: SUCCEED | FAIL, a: callback, b: stack }
-  , mailbox : [msg]
-  }
+let working = false;
+const queue = [];
 
-*/
+function _Scheduler_enqueue(proc) {
+    queue.push(proc);
 
+    if (working) {
+        return;
+    }
 
-var _Scheduler_working = false;
-var _Scheduler_queue = [];
+    working = true;
 
+    let next = queue.shift();
 
-function _Scheduler_enqueue(proc)
-{
-	_Scheduler_queue.push(proc);
-	if (_Scheduler_working)
-	{
-		return;
-	}
-	_Scheduler_working = true;
-	while (proc = _Scheduler_queue.shift())
-	{
-		_Scheduler_step(proc);
-	}
-	_Scheduler_working = false;
+    while (next) {
+        _Scheduler_step(proc);
+
+        next = queue.shift();
+    }
+
+    working = false;
 }
 
 
-function _Scheduler_step(proc)
-{
-	while (proc.__root)
-	{
-		var rootTag = proc.__root.$;
-		if (rootTag === __1_SUCCEED || rootTag === __1_FAIL)
-		{
-			while (proc.__stack && proc.__stack.$ !== rootTag)
-			{
-				proc.__stack = proc.__stack.__rest;
-			}
-			if (!proc.__stack)
-			{
-				return;
-			}
-			proc.__root = proc.__stack.__callback(proc.__root.__value);
-			proc.__stack = proc.__stack.__rest;
-		}
-		else if (rootTag === __1_BINDING)
-		{
-			proc.__root.__kill = proc.__root.__callback(function(newRoot) {
-				proc.__root = newRoot;
-				_Scheduler_enqueue(proc);
-			});
-			return;
-		}
-		else if (rootTag === __1_RECEIVE)
-		{
-			if (proc.__mailbox.length === 0)
-			{
-				return;
-			}
-			proc.__root = proc.__root.__callback(proc.__mailbox.shift());
-		}
-		else // if (rootTag === __1_AND_THEN || rootTag === __1_ON_ERROR)
-		{
-			proc.__stack = {
-				$: rootTag === __1_AND_THEN ? __1_SUCCEED : __1_FAIL,
-				__callback: proc.__root.__callback,
-				__rest: proc.__stack
-			};
-			proc.__root = proc.__root.__task;
-		}
-	}
+function _Scheduler_step(proc) {
+    while (proc.__root) {
+        const rootTag = proc.__root.$;
+
+        if (rootTag === __1_SUCCEED || rootTag === __1_FAIL) {
+            while (proc.__stack && proc.__stack.$ !== rootTag) {
+                proc.__stack = proc.__stack.__rest;
+            }
+            if (!proc.__stack) {
+                return;
+            }
+            proc.__root = proc.__stack.__callback(proc.__root.__value);
+            proc.__stack = proc.__stack.__rest;
+        } else if (rootTag === __1_BINDING) {
+            proc.__root.__kill = proc.__root.__callback(newRoot => {
+                proc.__root = newRoot;
+                _Scheduler_enqueue(proc);
+            });
+
+            return;
+        } else if (rootTag === __1_RECEIVE) {
+            if (proc.__mailbox.length === 0) {
+                return;
+            }
+            proc.__root = proc.__root.__callback(proc.__mailbox.shift());
+        } else /* if (rootTag === __1_AND_THEN || rootTag === __1_ON_ERROR) */ {
+            proc.__stack = {
+                $: rootTag === __1_AND_THEN ? __1_SUCCEED : __1_FAIL,
+                __callback: proc.__root.__callback,
+                __rest: proc.__stack
+            };
+            proc.__root = proc.__root.__task;
+        }
+    }
 }
