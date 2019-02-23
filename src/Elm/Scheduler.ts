a57594775e3957fde3fb4a5db63b8ea2fcee38b6
Anton Ovechkin <owanturist@gmail.com>
2019-02-23 12:50:03 +0700

    Add types for Scheduler

--- src/Elm/Scheduler.ts
+++ src/Elm/Scheduler.ts
@@ -1,118 +1,200 @@
-// T A S K S
+/**
+ * T A S K S
+ */
 
-const __1_SUCCEED = 'SUCCEED';
-const __1_FAIL = 'FAIL';
-const __1_BINDING = 'BINDING';
-const __1_AND_THEN = 'AND_THEN';
-const __1_ON_ERROR = 'ON_ERROR';
-const __1_RECEIVE = 'RECEIVE';
+export type Task<E, T>
+    = Succeed<T>
+    | Fail<E>
+    | Binding<E, T>
+    | AndThen<E, T>
+    | OnError<E, T>
+    | Receive<E, T>
+    ;
 
-
-export function succeed(value) {
-    return {
-        $: __1_SUCCEED,
-        __value: value
-    };
+interface Succeed<T> {
+    $: '_TASK__SUCCEED_';
+    __value: T;
 }
 
-export function fail(error) {
-    return {
-        $: __1_FAIL,
-        __value: error
-    };
+export const succeed = <T>(value: T): Task<never, T> => ({
+    $: '_TASK__SUCCEED_',
+    __value: value
+});
+
+
+interface Fail<E> {
+    $: '_TASK__FAIL_';
+    __error: E;
 }
 
-export function binding(callback) {
-    return {
-        $: __1_BINDING,
-        __callback: callback,
-        __kill: null
-    };
+export const fail = <E>(error: E): Task<E, never> => ({
+    $: '_TASK__FAIL_',
+    __error: error
+});
+
+
+interface Binding<E, T> {
+    $: '_TASK__BINDING';
+    __kill: void | (() => void);
+    __callback(bind: (task: Task<E, T>) => void): void | (() => void);
 }
 
-export function andThen(callback, task) {
-    return {
-        $: __1_AND_THEN,
-        __callback: callback,
-        __task: task
-    };
+export const binding = <E, T>(callback: (bind: (task: Task<E, T>) => void) => void | (() => void)): Task<E, T> => ({
+    $: '_TASK__BINDING',
+    __kill: undefined,
+    __callback: callback
+});
+
+
+interface AndThen<E, T> {
+    $: '_TASK__AND_THEN_';
+    __task: Task<E, T>;
+    __callback<R>(value: T): Task<E, R>;
 }
 
-export function onError(callback, task) {
-    return {
-        $: __1_ON_ERROR,
-        __callback: callback,
-        __task: task
-    };
+export const andThen = <E, T>(
+    callback: <R>(value: T) => Task<E, R>,
+    task: Task<E, T>
+): Task<E, T> => ({
+    $: '_TASK__AND_THEN_',
+    __task: task,
+    __callback: callback
+});
+
+
+interface OnError<E, T> {
+    $: '_TASK__ON_ERROR_';
+    __task: Task<E, T>;
+    __callback<S>(error: E): Task<S, T>;
 }
 
-export function receive(callback) {
-    return {
-        $: __1_RECEIVE,
-        __callback: callback
-    };
+export const onError = <E, T>(
+    callback: <S>(error: E) => Task<S, T>,
+    task: Task<E, T>
+): Task<E, T> => ({
+    $: '_TASK__ON_ERROR_',
+    __task: task,
+    __callback: callback
+});
+
+
+interface Receive<E, T> {
+    $: '_TASK__RECEIVE_';
+    __callback<M>(msg: M): Task<E, T>;
 }
 
+export const receive = <E, T>(callback: <M>(msg: M) => Task<E, T>): Task<E, T> => ({
+    $: '_TASK__RECEIVE_',
+    __callback: callback
+});
 
-// P R O C E S S E S
+/**
+ * S T A C K
+ */
 
-const __2_PROCESS = 'PROCESS';
+type Stack<E, T>
+    = Ok<E, T>
+    | Err<E, T>
+    ;
+
+interface Ok<E, T> {
+    $: '_STACK_OK_';
+    __rest?: Stack<E, T>;
+    __callback<R>(value: T): Task<E, R>;
+}
+
+const ok = <E, T>(
+    callback: <R>(value: T) => Task<E, R>,
+    rest?: Stack<E, T>
+): Stack<E, T> => ({
+    $: '_STACK_OK_',
+    __rest: rest,
+    __callback: callback
+});
+
+
+interface Err<E, T> {
+    $: '_STACK_ERR_';
+    __rest?: Stack<E, T>;
+    __callback<S>(error: E): Task<S, T>;
+}
+
+const err = <E, T>(
+    callback: <S>(error: E) => Task<S, T>,
+    rest?: Stack<E, T>
+): Stack<E, T> => ({
+    $: '_STACK_ERR_',
+    __rest: rest,
+    __callback: callback
+});
+
+/**
+ * P R O C E S S
+ */
 
 let GUID = 0;
 
-export function rawSpawn(task) {
-    const proc = {
-        $: __2_PROCESS,
-        __id: GUID++,
-        __root: task,
-        __stack: null,
-        __mailbox: []
-    };
+interface Process<E = unknown, T = unknown, M = unknown> {
+    __id: number;
+    __root?: Task<E, T>;
+    __stack?: Stack<E, T>;
+    __mailbox: Array<M>;
+}
+
+const process = <E, T>(root: Task<E, T>): Process<E, T> => ({
+    __id: GUID++,
+    __root: root,
+    __mailbox: []
+});
+
+const rawSpawn = <E, T>(task: Task<E, T>): Process<E, T> => {
+    const proc = process(task);
 
     _enqueue(proc);
 
     return proc;
-}
+};
 
-export function spawn(task) {
-    return binding(callback => {
-        callback(succeed(rawSpawn(task)));
-    });
-}
-
-export function rawSend(proc, msg) {
+const rawSend = <E, T, M>(proc: Process<E, T, M>, msg: M): void => {
     proc.__mailbox.push(msg);
     _enqueue(proc);
-}
+};
 
-export function send(proc, msg) {
-    return binding(callback => {
+export const spawn = <E, T>(task: Task<E, T>): Task<E, Process<E, T>> => binding(
+    (callback: (task: Task<E, Process<E, T>>) => void): void => {
+        callback(succeed(rawSpawn(task)));
+    }
+);
+
+export const send = <E, T, M>(proc: Process<E, T, M>, msg: M): Task<E, void> => binding(
+    (callback: (task: Task<E, void>) => void): void => {
         rawSend(proc, msg);
-        callback(succeed(null));
-    });
-}
+        callback(succeed(undefined));
+    }
+);
 
-export function kill(proc) {
-    return binding(callback => {
+export const kill = <E, T>(proc: Process<E, T>): Task<never, void> => binding(
+    (callback: (task: Task<never, void>) => void): void => {
         const task = proc.__root;
 
-        if (task.$ === __1_BINDING && task.__kill) {
+        if (task && task.$ === '_TASK__BINDING' && task.__kill) {
             task.__kill();
         }
 
-        proc.__root = null;
+        proc.__root = undefined;
 
-        callback(succeed(null));
-    });
-}
+        callback(succeed(undefined));
+    }
+);
 
+/**
+ * S T E P
+ */
 
-// S T E P
-
+const _queue: Array<Process> = [];
 let _working = false;
-const _queue = [];
 
-function _enqueue(proc) {
+function _enqueue<E, T, M>(proc: Process<E, T, M>): void {
     _queue.push(proc);
 
     if (_working) {
@@ -132,104 +214,70 @@ function _enqueue(proc) {
     _working = false;
 }
 
-
-function _step(proc) {
+function _step<E, T, M>(proc: Process<E, T, M>): void {
     while (proc.__root) {
-        const rootTag = proc.__root.$;
+        switch (proc.__root.$) {
+            case '_TASK__SUCCEED_': {
+                while (proc.__stack && proc.__stack.$ !== '_STACK_OK_') {
+                    proc.__stack = proc.__stack.__rest;
+                }
 
-        if (rootTag === __1_SUCCEED || rootTag === __1_FAIL) {
-            while (proc.__stack && proc.__stack.$ !== rootTag) {
+                if (!proc.__stack) {
+                    return;
+                }
+
+                proc.__root = proc.__stack.__callback(proc.__root.__value);
                 proc.__stack = proc.__stack.__rest;
-            }
-            if (!proc.__stack) {
-                return;
-            }
-            proc.__root = proc.__stack.__callback(proc.__root.__value);
-            proc.__stack = proc.__stack.__rest;
-        } else if (rootTag === __1_BINDING) {
-            proc.__root.__kill = proc.__root.__callback(newRoot => {
-                proc.__root = newRoot;
-                _enqueue(proc);
-            });
 
-            return;
-        } else if (rootTag === __1_RECEIVE) {
-            if (proc.__mailbox.length === 0) {
+                break;
+            }
+
+            case '_TASK__FAIL_': {
+                while (proc.__stack && proc.__stack.$ !== '_STACK_ERR_') {
+                    proc.__stack = proc.__stack.__rest;
+                }
+
+                if (!proc.__stack) {
+                    return;
+                }
+
+                proc.__root = proc.__stack.__callback(proc.__root.__error);
+                proc.__stack = proc.__stack.__rest;
+
+                break;
+            }
+
+            case '_TASK__BINDING': {
+                proc.__root.__kill = proc.__root.__callback((newRoot: Task<E, T>): void => {
+                    proc.__root = newRoot;
+
+                    _enqueue(proc);
+                });
+
                 return;
             }
-            proc.__root = proc.__root.__callback(proc.__mailbox.shift());
-        } else {
-            proc.__stack = {
-                $: rootTag === __1_AND_THEN ? __1_SUCCEED : __1_FAIL,
-                __callback: proc.__root.__callback,
-                __rest: proc.__stack
-            };
-            proc.__root = proc.__root.__task;
+
+            case '_TASK__RECEIVE_': {
+                const msg = proc.__mailbox.shift();
+
+                if (msg === undefined) {
+                    return;
+                }
+
+                proc.__root = proc.__root.__callback(msg);
+
+                break;
+            }
+
+            case '_TASK__AND_THEN_': {
+                proc.__stack = ok(proc.__root.__callback, proc.__stack);
+
+                break;
+            }
+
+            case '_TASK__ON_ERROR_': {
+                proc.__stack = err(proc.__root.__callback, proc.__stack);
+            }
         }
     }
 }
-
-/*
-
-let SchedulerWorking = false;
-const SchedulerQueue = [];
-
-
-function _Scheduler_enqueue(proc) {
-    SchedulerQueue.push(proc);
-
-    if (SchedulerWorking) {
-        return;
-    }
-
-    SchedulerWorking = true;
-
-    let next = SchedulerQueue.shift();
-
-    while (next) {
-        _Scheduler_step(proc);
-
-        next = SchedulerQueue.shift();
-    }
-
-    SchedulerWorking = false;
-}
-
-
-function _Scheduler_step(proc) {
-    while (proc.__root) {
-        const rootTag = proc.__root.$;
-
-        if (rootTag === __1_SUCCEED || rootTag === __1_FAIL) {
-            while (proc.__stack && proc.__stack.$ !== rootTag) {
-                proc.__stack = proc.__stack.__rest;
-            }
-            if (!proc.__stack) {
-                return;
-            }
-            proc.__root = proc.__stack.__callback(proc.__root.__value);
-            proc.__stack = proc.__stack.__rest;
-        } else if (rootTag === __1_BINDING) {
-            proc.__root.__kill = proc.__root.__callback(newRoot => {
-                proc.__root = newRoot;
-                _Scheduler_enqueue(proc);
-            });
-
-            return;
-        } else if (rootTag === __1_RECEIVE) {
-            if (proc.__mailbox.length === 0) {
-                return;
-            }
-            proc.__root = proc.__root.__callback(proc.__mailbox.shift());
-        } else {
-            proc.__stack = {
-                $: rootTag === __1_AND_THEN ? __1_SUCCEED : __1_FAIL,
-                __callback: proc.__root.__callback,
-                __rest: proc.__stack
-            };
-            proc.__root = proc.__root.__task;
-        }
-    }
-}
-
-*/
