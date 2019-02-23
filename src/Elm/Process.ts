9571989ef8929c52fbbf997d70d24db81499ef75
Anton Ovechkin <owanturist@gmail.com>
2019-02-23 13:41:37 +0700

    Add typings for Process

--- src/Elm/Process.ts
+++ src/Elm/Process.ts
@@ -1,13 +1,17 @@
 import * as Scheduler from './Scheduler';
 
-export function sleep(time) {
+export type ID = Scheduler.Process;
+
+export function sleep(time: number): Scheduler.Task<never, void> {
     return Scheduler.binding(callback => {
         const id = setTimeout(() => {
-            callback(Scheduler.succeed(null));
+            callback(Scheduler.succeed(undefined));
         }, time);
 
         return () => { clearTimeout(id); };
     });
 }
 
-export const spawn = Scheduler.spawn;
+export const spawn: <E, T>(task: Scheduler.Task<E, T>) => Scheduler.Task<E, ID> = Scheduler.spawn;
+
+export const kill: (process: ID) => Scheduler.Task<never, void> = Scheduler.kill;
