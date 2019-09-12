5bc4971376ee4818acc8d0f0424823fe9aacf095
Anton Ovechkin <owanturist@gmail.com>
2019-09-12 17:27:35 +0700

    Json/Decode refactoring (#10)
    * Remove Chainable
    
    * Decode.maybe → Decode.optional
    
    * Make Decode.optional acts like it is expected
    
    * Design new OOP friendly API
    
    * Reset tests
    
    * Implement basic Primitives
    
    * Implement Optional with Nullable
    
    * Implement Path and Field
    
    * Implement OptionalPath with OptioanlField
    
    * Implement field().optional
    
    * Update Field expect message
    
    * Implement OptionalPath and Map
    
    * Implement index
    
    * DRY *Field classes
    
    * Implement OptionalIndex
    
    * Reorganize tests
    
    * Add more index tests
    
    * Test all primitive and Value
    
    * Implement fail and succeed
    
    * Implement Props
    
    * Implement oneOf
    
    * Implement List
    
    * Implement keyValue
    
    * Implement dict
    
    * Implement lazy and Chain
    
    * Implement fromEither and fromMaybe
    
    * Implement at
    
    * Implement optionalAt
    
    * Add map tests
    
    * Add chain tests
    
    * Add decodeJSON tests
    
    * Add tests for field+something
    
    * Add tests to index++
    
    * Add more cases to optional.index
    
    * Add tests for at++
    
    * Add more optional tests
    
    * Add Json.Decode.field(name).optional.field(name).of(decoder)
    
    * Add Json.Decode.field(name).optional.index(position).of(decoder)
    
    * Add Json.Decode.field(name).optional.at(path).of(decoder)
    
    * Add index + (field|index|at) integration tests
    
    * Add at integrations tests
    
    * Made optional tests more detailed
    
    * Add optional cases for map and chain
    
    * Hide decodaAs under protected
    
    * Make .optional more greedy
    
    * Return optional.props back
    
    * Made optional|reuired casts of Index and Field
    
    * Manage imports
    
    * Split Decode.Error
    
    * Simplify Decode/Error
    
    * Fix Error stringify with undefined source
    
    * Split Decode into main and index
    
    * Decode.props → Decode.shape
    
    * Add Decode.enums
    
    * Add Decode/Error documentation
    
    * Add docs to primitives and prototype methods
    
    * Add docs for all top level Decode
    
    * Add docs for Optional
    
    * Add docs for Path
    
    * Move all docs to index

---
+++ src/Json/Decode/Error/index.ts
@@ -0,0 +1,98 @@
+import {
+    Cata
+} from '../../../Basics';
+
+import * as _ from './Error';
+
+/**
+ * A structured error describing exactly how the decoder failed.
+ * You can use this to create more elaborate visualizations of a decoder problem.
+ * For example, you could show the entire JSON object and show the part causing the failure in red.
+ */
+export interface Error {
+    /**
+     * Match the current `Error` to provided pattern.
+     *
+     * @param pattern Pattern matching.
+     */
+    cata<R>(pattern: Pattern<R>): R;
+
+    /**
+     * Convert a decoding `Error` into a `string` that is nice for debugging.
+     *
+     * @param indent Counter of spaces for JSON.stringify of an `input`.
+     */
+    stringify(indent: number): string;
+}
+
+export namespace Error {
+    /**
+     * Pattern for matching `Error` variants.
+     */
+    export type Pattern<R> = Cata<{
+        OneOf(errors: Array<Error>): R;
+        Field(name: string, error: Error): R;
+        Index(position: number, error: Error): R;
+        Failure(message: string, source: unknown): R;
+    }>;
+
+
+    /**
+     * Collects a batch of `Error`s come from `Decode.oneOf` and `Decode.enums`.
+     *
+     * @param errors List of `Error`s
+     */
+    export const OneOf = _.OneOf;
+
+    /**
+     * Collects an `Error` comes from `Decode.field` and `Decode.at`
+     *
+     * @param name  Field name triggers the `error`
+     * @param error Nested field `Error`
+     */
+    export const Field = _.Field;
+
+    /**
+     * Collects an `Error` comes from `Decode.index` and `Decode.at`
+     *
+     * @param position Index position triggers the `error`
+     * @param error    Nested field `Error`
+     */
+    export const Index = _.Index;
+
+    /**
+     * Collects an `Error` comes from `Decode.field` and `Decode.at`
+     *
+     * @param message Description of the error.
+     * @param source  JS source input failed while decoding.
+     */
+    export const Failure = _.Failure;
+}
+
+/**
+ * @alias `Error.Pattern`
+ */
+export type Pattern<R> = Error.Pattern<R>;
+
+/**
+ * @alias `Error.OneOf`
+ */
+export const OneOf = Error.OneOf;
+
+/**
+ * @alias `Error.Field`
+ */
+export const Field = Error.Field;
+
+/**
+ * @alias `Error.Index`
+ */
+export const Index = Error.Index;
+
+/**
+ * @alias `Error.Failure`
+ */
+export const Failure = Error.Failure;
+
+
+export default Error;
