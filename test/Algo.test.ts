27b79f57673be714fb6f4e546a8ccd13f4b196e7
Anton Ovechkin <owanturist@gmail.com>
2019-12-13 16:19:55 +0700

    Implement mergesort

--- test/Algo.test.ts
+++ test/Algo.test.ts
@@ -70,19 +70,19 @@ test('Algo.range', t => {
 function shuffle<T>(arr: Array<T>): Array<T>;
 function shuffle<T>(lo: number, hi: number, arr: Array<T>): Array<T>;
 function shuffle<T>(...params: [ Array<T> ] | [ number, number, Array<T>]): Array<T> {
-    const [ lo, hi, arr ] = bounds(...params);
+    const [ lo, hi, source ] = bounds(...params);
 
-    const acc = arr.slice();
+    const copy = source.slice();
 
     for (let i = lo; i < hi; i++) {
         swap(
             i + Math.floor(Math.random() * (hi - i)),
             i,
-            acc
+            copy
         );
     }
 
-    return acc;
+    return copy;
 }
 
 test('Algo.shuffle', t => {
@@ -175,6 +175,95 @@ test('Algo.quicksort', t => {
     const _3 = [ 7, 1, 0, 2, 8, 3, 6, 4, 9, 5 ];
     t.deepEqual(
         quicksort(3, 7, _3),
+        [ 7, 1, 0, 2, 3, 6, 8, 4, 9, 5 ]
+    );
+});
+
+// 0 10
+// 0 5 | 5 10
+// 0 2 | 2 5 || 5 7 | 7 10
+// 0 1 | 1 2 || 2 3 | 3 5 || 5 6 | 6 7 || 7 | 8 || 8 | 10
+// 0 0 | 1 1 || 1 1 | 2 2 || 2 2 | 3 3 || 3 4 | 4 5 || 5 5 | 6 6 ||
+// 0 1 2 7 8 3 6 4 9 5
+// 0 1 7 2 8 3 6 4 9 5
+
+//      v
+// tmp: 1 3 0 2
+//          ^
+// res: 3 1 0 2
+//      ^
+
+function mergesort(arr: Array<number>): Array<number>;
+function mergesort(lo: number, hi: number, arr: Array<number>): Array<number>;
+function mergesort(...params: [ Array<number> ] | [ number, number, Array<number>]): Array<number> {
+    const [ lo, hi, arr ] = bounds(...params);
+
+    const copy = arr.slice();
+
+    mergesortRoutine(lo, hi, arr.slice(), copy);
+
+    return copy;
+}
+
+const mergesortRoutine = (lo: number, hi: number, tmp: Array<number>, res: Array<number>): void => {
+    // prevent sorting a single or empty element
+    if (hi - lo < 2) {
+        return;
+    }
+
+    const middle = lo + Math.floor((hi - lo) / 2);
+
+    mergesortRoutine(lo, middle, res, tmp);
+    mergesortRoutine(middle, hi, res, tmp);
+
+    for (let i = lo, left = lo, right = middle; left < middle || right < hi; i++) {
+        if (left >= middle) {
+            res[ i ] = tmp[ right++ ];
+        } else if (right >= hi || tmp[ left ] < tmp[ right ]) {
+            res[ i ] = tmp[ left++ ];
+        } else {
+            res[ i ] = tmp[ right++ ];
+        }
+    }
+};
+
+test('Algo.mergesort', t => {
+    t.deepEqual(
+        mergesort([]),
+        []
+    );
+
+    t.deepEqual(
+        mergesort([ 0 ]),
+        [ 0 ]
+    );
+
+    t.deepEqual(
+        mergesort([ 3, 1, 0, 2 ]),
+        range(0, 3)
+    );
+
+    const _0 = range(0, 9);
+    t.deepEqual(
+        mergesort(shuffle(_0)),
+        _0
+    );
+
+    const _1 = range(0, 999);
+    t.deepEqual(
+        mergesort(shuffle(_1)),
+        _1
+    );
+
+    const _2 = [ 0, 0, 0, 0, 1, 2, 3, 3, 3, 4, 5, 5, 6, 7, 8, 8, 8, 8, 9 ];
+    t.deepEqual(
+        mergesort(shuffle(_2)),
+        _2
+    );
+
+    const _3 = [ 7, 1, 0, 2, 8, 3, 6, 4, 9, 5 ];
+    t.deepEqual(
+        mergesort(3, 7, _3),
         [ 7, 1, 0, 2, 3, 6, 8, 4, 9, 5 ]
     );
 });
