b671426cb5686e144bce8d743cdcfe5a9198fb6e
Anton Ovechkin <owanturist@gmail.com>
2019-12-13 17:17:54 +0700

    Make mergesort stable

--- test/Algo.test.ts
+++ test/Algo.test.ts
@@ -1,5 +1,12 @@
 import test from 'ava';
 
+interface Sorting {
+    <T extends number | string>(arr: Array<T>): Array<T>;
+    <T>(arr: Array<T>, comparator: Comparator<T>): Array<T>;
+    <T extends number | string>(lo: number, hi: number, arr: Array<T>): Array<T>;
+    <T>(lo: number, hi: number, arr: Array<T>, comparator: Comparator<T>): Array<T>;
+}
+
 type Comparator<T> = (left: T, right: T) => number;
 
 const defaultComparator = <T extends string | number>(left: T, right: T): number => {
@@ -31,6 +38,29 @@ const bounds = <T>(...params: [ Array<T> ] | [ number, number, Array<T> ]): [ nu
         : params;
 
     return [ Math.max(0, lo), Math.min(arr.length, hi), arr ];
+};
+
+const attributes = <T>(params
+    : [ Array<T> ]
+    | [ Array<T>, Comparator<T> ]
+    | [ number, number, Array<T> ]
+    | [ number, number, Array<T>, Comparator<T> ]
+): [ number, number, Array<T>, Comparator<T> ] => {
+    if (params.length === 2) {
+        const [ lo, hi, arr ] = bounds(params[ 0 ]);
+
+        return [ lo, hi, arr, params[ 1 ]];
+    }
+
+    if (params.length === 4) {
+        const [ lo, hi, arr ] = bounds(params[ 0 ], params[ 1 ], params[ 2 ]);
+
+        return [ lo, hi, arr, params[ 3 ]];
+    }
+
+    const [ lo, hi, arr ] = bounds(...params);
+
+    return [ lo, hi, arr, defaultComparator as Comparator<T> ];
 };
 
 const range = (start: number, end: number): Array<number> => {
@@ -107,36 +137,20 @@ test('Algo.shuffle', t => {
     t.deepEqual(arr, shuffled.slice().sort((a, b) => a - b));
 });
 
-function quicksort<T extends number | string>(arr: Array<T>): Array<T>;
-function quicksort<T>(arr: Array<T>, comparator: Comparator<T>): Array<T>;
-function quicksort<T extends number | string>(lo: number, hi: number, arr: Array<T>): Array<T>;
-function quicksort<T>(lo: number, hi: number, arr: Array<T>, comparator: Comparator<T>): Array<T>;
-function quicksort<T>(
-    ...params: [ Array<T> ]
-             | [ Array<T>, Comparator<T> ]
-             | [ number, number, Array<T> ]
-             | [ number, number, Array<T>, Comparator<T> ]
-): Array<T> {
-    const [ lo, hi, arr ] = params.length === 2
-        ? bounds(params[ 0 ])
-        : params.length === 4
-            ? bounds(params[ 0 ], params[ 1 ], params[ 2 ])
-            : bounds(...params)
-            ;
-
-    const cmp = params.length === 2
-        ? params[ 1 ]
-        : params.length === 4
-            ? params[ 3 ]
-            : defaultComparator as Comparator<unknown> as Comparator<T>
-            ;
+const quicksort: Sorting = <T>(...params
+    : [ Array<T> ]
+    | [ Array<T>, Comparator<T> ]
+    | [ number, number, Array<T> ]
+    | [ number, number, Array<T>, Comparator<T> ]
+): Array<T> => {
+    const [ lo, hi, arr, cmp ] = attributes(params);
 
     const copy = shuffle(lo, hi, arr);
 
     quicksortRoutine(lo, hi, copy, cmp);
 
     return copy;
-}
+};
 
 const quicksortRoutine = <T>(lo: number, hi: number, arr: Array<T>, cmp: Comparator<T>): void => {
     if (lo >= hi) {
@@ -215,19 +229,22 @@ test('Algo.quicksort', t => {
     );
 });
 
-function mergesort(arr: Array<number>): Array<number>;
-function mergesort(lo: number, hi: number, arr: Array<number>): Array<number>;
-function mergesort(...params: [ Array<number> ] | [ number, number, Array<number>]): Array<number> {
-    const [ lo, hi, arr ] = bounds(...params);
+const mergesort: Sorting = <T>(...params
+    : [ Array<T> ]
+    | [ Array<T>, Comparator<T> ]
+    | [ number, number, Array<T> ]
+    | [ number, number, Array<T>, Comparator<T> ]
+): Array<T> => {
+    const [ lo, hi, arr, cmp ] = attributes(params);
 
     const copy = arr.slice();
 
-    mergesortRoutine(lo, hi, arr.slice(), copy);
+    mergesortRoutine(lo, hi, arr.slice(), copy, cmp);
 
     return copy;
-}
+};
 
-const mergesortRoutine = (lo: number, hi: number, tmp: Array<number>, res: Array<number>): void => {
+const mergesortRoutine = <T>(lo: number, hi: number, tmp: Array<T>, res: Array<T>, cmp: Comparator<T>): void => {
     // prevent sorting a single or empty element
     if (hi - lo < 2) {
         return;
@@ -235,13 +252,13 @@ const mergesortRoutine = (lo: number, hi: number, tmp: Array<number>, res: Array
 
     const middle = lo + Math.floor((hi - lo) / 2);
 
-    mergesortRoutine(lo, middle, res, tmp);
-    mergesortRoutine(middle, hi, res, tmp);
+    mergesortRoutine(lo, middle, res, tmp, cmp);
+    mergesortRoutine(middle, hi, res, tmp, cmp);
 
     for (let i = lo, left = lo, right = middle; left < middle || right < hi; i++) {
         if (left >= middle) {
             res[ i ] = tmp[ right++ ];
-        } else if (right >= hi || tmp[ left ] < tmp[ right ]) {
+        } else if (right >= hi || cmp(tmp[ left ], tmp[ right ]) <= 0) {
             res[ i ] = tmp[ left++ ];
         } else {
             res[ i ] = tmp[ right++ ];
@@ -261,31 +278,57 @@ test('Algo.mergesort', t => {
     );
 
     t.deepEqual(
-        mergesort([ 3, 1, 0, 2 ]),
-        range(0, 3)
+        mergesort(shuffle(range(0, 9))),
+        range(0, 9)
     );
 
-    const _0 = range(0, 9);
     t.deepEqual(
-        mergesort(shuffle(_0)),
-        _0
+        mergesort(shuffle(range(0, 999))),
+        range(0, 999)
     );
 
-    const _1 = range(0, 999);
+    t.deepEqual(
+        mergesort(shuffle(range(999, 0)), (a, b) => b - a),
+        range(999, 0)
+    );
+
+    const _1 = [ 0, 0, 0, 0, 1, 2, 3, 3, 3, 4, 5, 5, 6, 7, 8, 8, 8, 8, 9 ];
     t.deepEqual(
         mergesort(shuffle(_1)),
         _1
     );
 
-    const _2 = [ 0, 0, 0, 0, 1, 2, 3, 3, 3, 4, 5, 5, 6, 7, 8, 8, 8, 8, 9 ];
+    t.deepEqual(
+        mergesort(3, 7, [ 7, 1, 0, 2, 8, 3, 6, 4, 9, 5 ]),
+        [ 7, 1, 0, 2, 3, 6, 8, 4, 9, 5 ]
+    );
+
+    const _2 = 'ABCDEFGH'.split('');
     t.deepEqual(
         mergesort(shuffle(_2)),
         _2
     );
 
-    const _3 = [ 7, 1, 0, 2, 8, 3, 6, 4, 9, 5 ];
+    const _4 = [
+        { x: 'A', y: 0 },
+        { x: 'A', y: 1 },
+        { x: 'A', y: 2 },
+        { x: 'A', y: 4 },
+        { x: 'A', y: 9 },
+        { x: 'B', y: 0 },
+        { x: 'B', y: 1 },
+        { x: 'B', y: 2 },
+        { x: 'B', y: 2 },
+        { x: 'C', y: 6 },
+        { x: 'C', y: 7 },
+        { x: 'D', y: 0 }
+    ];
+
     t.deepEqual(
-        mergesort(3, 7, _3),
-        [ 7, 1, 0, 2, 3, 6, 8, 4, 9, 5 ]
+        mergesort(
+            mergesort(shuffle(_4), (a, b) => a.y - b.y),
+            (a, b) => a.x.charCodeAt(0) - b.x.charCodeAt(0)
+        ),
+        _4
     );
 });
